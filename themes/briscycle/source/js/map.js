f9f1b16c85dc414f4d9d13346f60a72954e7e54a
Ash Kyd <ash@kyd.com.au>
2019-08-10 12:35:58 +0200

    remove console.log & maybe fix card ad

--- themes/briscycle/source/js/map.js
+++ themes/briscycle/source/js/map.js
@@ -1,7 +1,7 @@
 var MAPBOXTOKEN = "pk.eyJ1IjoiYXNoa3lkIiwiYSI6IkVGMTRITjgifQ.qMQ0S2B-091S7U4cXhRhSg";
 var MAPBOXLAYER = "ashkyd.l73nmeac";
 var TILES = "https://{s}.tiles.mapbox.com/v4/" + MAPBOXLAYER + "/{z}/{x}/{y}.png?access_token=" + MAPBOXTOKEN;
-console.log({json: document.querySelector('#map-config').innerText})
+// console.log({json: document.querySelector('#map-config').innerText})
 const config = JSON.parse(document.querySelector('#map-config').innerText);
 const mymap = L.map('map', {
   scrollWheelZoom: false
