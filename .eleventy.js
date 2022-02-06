cec88697d26f346129f2d682d4f0a75583c2d9cf
Ash Kyd <ash@kyd.com.au>
2022-02-06 22:31:47 +1000

    mess around with ads, they're not working

--- .eleventy.js
+++ .eleventy.js
@@ -17,14 +17,20 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addShortcode("ad", function () {
-    return `<aside style="min-height: 200px"><ins class="adsbygoogle"
+    return `<aside style="min-height: 200px">
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0801621623358731"
+crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
 style="display:block; text-align:center;"
 data-ad-layout="in-article"
 data-ad-format="fluid"
 data-ad-client="ca-pub-0801621623358731"
 data-ad-slot="5298906050"></ins>
-<script defer src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0801621623358731"
-crossorigin="anonymous"></script>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 </aside>`;
   });
 
